5430b47047561fff09cbe4df5f855ed136d4ba46
v1c7or20 <victor.pena@utec.edu.pe>
2019-05-28 17:21:26 -0500

    heredado de html
    se agrega html heredados y algunos html para completar

--- Proyecto1/run.py
+++ Proyecto1/run.py
@@ -15,6 +15,16 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/ubicanos')
+def ubicanos():
+    return render_template('ubicanos.html')
+
+
+@app.route('/catalogo')
+def catalogo():
+    return render_template('catalogo.html')
+
+
 if __name__ == '__main__':
     app.secret_key = ".."
     app.run(debug = True ,port=8080, threaded=True, host=('127.0.0.1'))
