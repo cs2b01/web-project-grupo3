62cd2509f05e2042fb3a565347d4a208d6bc74ed
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-06-01 22:11:41 -0500

    Add files via upload

---
+++ Proyecto1/templates/catalogo.js
@@ -0,0 +1,48 @@
+$(function(){
+    var url = "http://127.0.0.1:8080/catalogo.html";
+
+
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url ,
+            insertUrl: url ,
+            updateUrl: url ,
+            deleteUrl: url ,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+        editing: {
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+        remoteOperations: {
+            sorting: true,
+            paging: true
+        },
+        paging: {
+            pageSize: 12
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "Nombre"
+        }, {
+            dataField: "Marca"
+        }, {
+            dataField: "Numero de serie"
+        }, {
+            dataField: "STOCK"
+        }, {
+            dataField: "Precio"
+        }, ],
+    }).dxDataGrid("instance");
+});
